c89281caba8fdc8a36df75a114dcd02410ec9367
Jose Sandoval Jimenez <62967903+Jose-Sand@users.noreply.github.com>
2026-05-02 15:04:16 -0500

    feat: add nomina-colombiana-python/__init__.py

---
+++ nomina-colombiana-python/__init__.py
@@ -0,0 +1,5 @@
+from .prestaciones_sociales import calcular_prestaciones_sociales
+from .aportes_pila import calcular_aportes_pila
+from .retencion_en_fuente import calcular_retencion_en_fuente
+
+__all__ = ['calcular_prestaciones_sociales', 'calcular_aportes_pila', 'calcular_retencion_en_fuente']
